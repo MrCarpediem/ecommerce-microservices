a0292f49b98c5264b48c61d93a7dc6948df4b4ee
Prem Prakash <ne.premprakash53@gmail.com>
2026-04-20 02:08:11 +0530

    fix: Remove directConnection=true to support Atlas mongodb+srv URIs

--- Backend/authService/src/config/db.js
+++ Backend/authService/src/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async (retryCount = 0) => {
   try {
     const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/auth-service';
     
-    await mongoose.connect(mongoUri, { serverSelectionTimeoutMS: 10000, family: 4, directConnection: true });
+    await mongoose.connect(mongoUri);
 
 
 
